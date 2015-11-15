d478403d60f0c4de87ff91dfc472c3e3a3f61b90
Gandhi <gansagar@10ddb1f0b560.ant.amazon.com>
2015-11-15 15:27:47 +0530

    getting list of movies from a folder & provide rating for each movie

--- imdb_rating.py
+++ imdb_rating.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
 def getRatingAndNoOfUsers(mov_name,mov_url):
 	url = 'http://www.imdb.com' + mov_url
@@ -10,27 +11,90 @@ def getRatingAndNoOfUsers(mov_name,mov_url):
 	no_of_users_rated = item[0].contents[1].text
 	print mov_name, rating , no_of_users_rated
 
+
+movielist = []
+
 def getMovieNames():
-	test
+	path = '/Users/gansagar/Desktop/backup/movies'
+	dirs = os.listdir(path)
+	uwords = ['240p','360p','480p','720p','1080p','rip','x264','bluray']
+	#fileext = ['.mp4','.avi','.mkv']
+	fnames = []
+	for fname in dirs:
+		temp = fname
+		fname = fname.lower()
+		if ".srt" in fname: #check for subtitles
+			continue
+		cpath = os.path.join(path, temp)
+		if os.path.isdir(cpath):  #check for a directory
+			fnames.append(fname)
+			#print fname
+		else:
+			fnames.append(fname[:-4]) #removing extension
+			#print fname[:-4]  
+	#print fnames
+	for fname in fnames:
+		for uword in uwords:
+			idx = fname.find(uword)
+			#print fname,uword,idx
+			if idx == -1:
+				continue
+			else:
+				while idx>=0:
+					if fname[idx]>='a' and fname[idx]<='z':
+						idx = idx - 1
+					# elif  fname[idx]=='[' or fname[idx]=='.' or fname[idx]=='_' or fname[idx]=='-':
+					# 	idx = idx - 1
+					# 	break
+					else:
+						break
+				fname = fname[:idx]
+			
+		idx1 = fname.find('[')
+		if idx1 != -1: 
+			idx2 = fname.find(']',idx1)
+			strg = fname[idx1+1:idx2]
+			strg = strg.strip()
+			#print "########################",strg
+			if not strg.isdigit():
+				fname = fname[:idx1] + fname[idx2+1:]
 
-base_url = 'http://www.imdb.com/find?q='
-movie_name = 'spy'
-url = base_url + movie_name + '&s=all'
-#print url
-r = requests.get(url)
+		idx1 = fname.find('(')
+		if idx1 != -1: 
+			idx2 = fname.find(')',idx1)
+			strg = fname[idx1+1:idx2]
+			strg = strg.strip()
+			#print "################",strg
+			if not strg.isdigit():
+				fname = fname[:idx1] + fname[idx2+1:]
+		movielist.append(fname)
+	#print movielist
 
-soup = BeautifulSoup(r.text,'html.parser')
 
-mov_list = soup.find_all("table",{"class":"findList"})
+getMovieNames()
 
-# print mov_list
-details = mov_list[0].find_all("td",{"class":"result_text"})
-#print details ,'\n'
+for movie_name in movielist:
+	base_url = 'http://www.imdb.com/find?q='
+	#movie_name = ' crazy heart (2010)'
+	url = base_url + movie_name + '&s=all'
+	#print url
+	r = requests.get(url)
 
-link = details[0].contents[1].get('href')
-mov_name = details[0].text
-# print mov_name,link,'\n'
-getRatingAndNoOfUsers(mov_name,link)
+	soup = BeautifulSoup(r.text,'html.parser')
+
+	try:
+		mov_list = soup.find_all("table",{"class":"findList"})
+
+		# print mov_list
+		details = mov_list[0].find_all("td",{"class":"result_text"})
+		#print details ,'\n'
+
+		link = details[0].contents[1].get('href')
+		mov_name = details[0].text
+		# print mov_name,link,'\n'
+		getRatingAndNoOfUsers(mov_name,link)
+	except:
+		pass
 
 
 
